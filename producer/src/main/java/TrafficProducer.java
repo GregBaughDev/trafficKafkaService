6b3999e44c3c812ec54ff557663e99d9e4a8e023
Greg <80447308+GregBDesign@users.noreply.github.com>
2023-02-09 21:22:27 +1100

    Add kafka connection

--- producer/src/main/java/TrafficProducer.java
+++ producer/src/main/java/TrafficProducer.java
@@ -1,12 +1,39 @@
 import api.TrafficAPI;
 import api.record.Response;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import producer.TrafficInfo;
+import producer.config.TrafficProducerConfig;
 
 public class TrafficProducer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("******Traffic producer******");
+
         TrafficAPI api = new TrafficAPI();
         Response response = api.getDataResponse();
 
-        System.out.println("Traffic producer");
-        System.out.println(response.features().get(0).geometry().geometries());
+        TrafficProducerConfig producerConfig = new TrafficProducerConfig();
+
+        while (true) {
+            response.features().stream().limit(10).forEach(feature -> {
+                ProducerRecord<String, TrafficInfo> trafficProducer = new ProducerRecord<>(
+                        "traffic_info", new TrafficInfo(
+                        feature.properties().closedRoadName(),
+                        feature.properties().startIntersectionRoadName(),
+                        feature.properties().localGovernmentArea(),
+                        feature.properties().eventType(),
+                        feature.properties().eventDueTo(),
+                        feature.properties().impact().direction(),
+                        feature.properties().impact().impactType(),
+                        feature.properties().impact().delay(),
+                        feature.properties().impact().speedLimitOnSite(),
+                        feature.properties().duration().start(),
+                        feature.properties().duration().end(),
+                        feature.properties().description()
+                )
+                );
+                producerConfig.producer().send(trafficProducer);
+            });
+            Thread.sleep(60000);
+        }
     }
 }
