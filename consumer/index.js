0d75e0c93fada5d1f4233c14430a503b745bcee3
Greg <80447308+GregBDesign@users.noreply.github.com>
2023-03-13 14:52:51 +1100

    add express

--- consumer/index.js
+++ consumer/index.js
@@ -1,4 +1,6 @@
+import express from 'express'
 import { Kafka } from 'kafkajs'
+const app = express()
 
 const kafka = new Kafka({
   clientId: 'traffic_info',
@@ -7,18 +9,30 @@ const kafka = new Kafka({
 
 const consumer = kafka.consumer({ groupId: 'traffic_info' })
 
+const data = []
 
-const run = async () => {
+const kafkaRun = async () => {
   await consumer.connect()
   await consumer.subscribe({ topic: 'traffic_info', fromBeginning: true })
-
+  // TO DO: The array needs to be reset
+  // TO DO: Use a ORM to map the responses?
+  data.splice(0)
   await consumer.run({
     eachMessage: async ({ _, __, message }) => {
       console.log({
         value: message.value.toString(),
       })
+      data.push(message.value.toString())
     }
   })
 }
 
-run()
+app.get('/info', (_, res) => {
+  res.status(200).json(data)
+})
+
+app.listen(8282, () => {
+  kafkaRun()
+  console.log('Serving on port 8282')
+})
+
