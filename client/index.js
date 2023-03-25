dd1c6a74b5f2ee88b10d4f894430aed6f92d4457
Greg <80447308+GregBDesign@users.noreply.github.com>
2023-03-25 14:43:04 +1100

    start to add info to ui

--- client/index.js
+++ client/index.js
@@ -1,12 +1,46 @@
-const test = document.getElementById('traffic-div')
+const trafficDiv = document.getElementById('traffic-div')
 
-test.textContent = "test"
-// Grab all the information from redis
+const trafficContainer = (info, parentElem) => {
+  const parent = document.createElement("div")
+  const leftDiv = document.createElement("div")
+  const h3 = document.createElement("h3")
+  const h4 = document.createElement("h4")
+  const h5 = document.createElement("h5")
+  const hr = document.createElement("hr")
 
-// For each append to the screen
+  parent.className = "container"
 
-const trafficContainer = () => {
-  const parent = document.createElement('div')
-  // add the information
-  // return the parent
+  h3.textContent = `Road: ${info.closedRoadName}`
+  h4.textContent = `Intersection road name: ${info.startIntersectionRoadName}`
+  h5.textContent = `Area: ${info.localGovernmentArea}`
+
+  const rightDiv = document.createElement("div")
+  const moreInfo = document.createElement("a")
+  moreInfo.innerHtml = "More Info"
+  moreInfo.href = "www.google.com"
+  rightDiv.append(moreInfo)
+  leftDiv.append(h3)
+  leftDiv.append(h4)
+  leftDiv.append(h5)
+  parent.append(leftDiv)
+  parent.append(rightDiv)
+  parent.append(hr)
+  parentElem.append(parent)
 }
+// Test
+const infotest = {
+  closedRoadName: "Rainbow Road",
+  startIntersectionRoadName: "Town Lane",
+  localGovernmentArea: "Port Phillip"
+}
+
+const infotest2 = {
+  closedRoadName: "Rainbow Road2",
+  startIntersectionRoadName: "Town Lanesx2",
+  localGovernmentArea: "Port Phillip2"
+}
+
+const data = [infotest, infotest2]
+// End test data
+
+data.map((elem) => trafficContainer(elem, trafficDiv))
--- client/main.js
+++ client/main.js
@@ -3,7 +3,8 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1000,
-    height: 800
+    height: 800,
+    resizable: false
   })
 
   win.loadFile('index.html')
